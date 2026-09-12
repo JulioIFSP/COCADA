5b84da1f2bbf76a9d7191ce22d0e0fb3ddc63b88
JulioIFSP <julioifsppc@gmail.com>
2026-09-11 21:54:40 -0300

    fix: alinhar dependências ao Node 18.16.1

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, 'public'));
+
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
